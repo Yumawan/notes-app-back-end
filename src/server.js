730488cf4edf298b1897749716ab2be789385ccc
Yumawan <kelvinanggayumawan21@gmail.com>
2024-05-28 16:43:15 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         // agar client dan server bisa saling komunikasi, diperlukan mekanisme "Cross-origin resource sharing (CORS)"
         routes: {
             cors: {
